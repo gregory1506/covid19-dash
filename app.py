3e17e7c2a61a05953479cc4884beb7188af29a97
gregory1506 <gjotomcat@gmail.com>
2020-08-19 23:33:12 -0400

    gfhgh

--- app.py
+++ app.py
@@ -317,7 +317,7 @@ def make_covid_map(country=None,date=None,cases=None):
                                         lon=df_tmp.Long,
                                         mode="markers",
                                         marker=go.scattermapbox.Marker(
-                                                size=math.log10(df_tmp[case]) ** (1/2),
+                                                size=df_tmp[case] ** (1/2),
                                                 color=COLORS[case],
                                                 opacity=0.7
                                         ),
